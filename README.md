3b9e92c5aa3acfef1e5f5bc7a7786f1436d3e8ff
Shamanth-GiT <shamanth.kuthpadi2003@gmail.com>
2025-04-25 14:19:56 -0400

    setup cont.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # KANs
+
+python -m pip install -r requirements.txt
+python -m task02_unsupervised.experiment
+
+conda create -n kanenv python=3.12.2  
---
+++ task02_unsupervised/experiment.py
@@ -0,0 +1,9 @@
+import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import sys
+from kan import *
+from utils import *
+
